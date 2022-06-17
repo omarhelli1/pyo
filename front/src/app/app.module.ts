6fc36b1ac62b20ebca2fafa235df53e05af10435
Paul Decoux <Decouxpaul1@gmail.com>
2022-06-17 16:47:35 +0200

    Creation du componsant theme-page et theme-card:tada:

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThemesPageComponent } from './pages/themes-page/themes-page.component';
+import { ThemeCardComponent } from './pages/themes-page/theme-card/theme-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [  
+    AppComponent,
+    ThemesPageComponent,
+    ThemeCardComponent
   ],
   imports: [
     BrowserModule,
