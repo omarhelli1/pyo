c79cb7393780044a639e4d4c3a4859f562150986
Paul Decoux <Decouxpaul1@gmail.com>
2022-06-22 14:04:53 +0200

    changement dans le appModule

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThemesPageComponent } from './pages/themes-page/themes-page.component';
+import { ThemeCardComponent } from './pages/themes-page/theme-card/theme-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [  
     AppComponent,
+    ThemesPageComponent,
+    ThemeCardComponent,
   ],
   imports: [
     BrowserModule,
