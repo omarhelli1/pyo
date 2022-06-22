b1ed5b8036869a1de3f9e36d422fb3cab6f400ae
Paul Decoux <Decouxpaul1@gmail.com>
2022-06-22 16:35:33 +0200

    feat: details formation composant:tada:

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 
 import { NavBarComponent } from './_commons/nav-bar/nav-bar.component';
 import { FooterComponent } from './_commons/footer/footer.component';
+import { ThemesPageComponent } from './pages/themes-page/themes-page.component';
+import { DetailsFormationComponent } from './pages/themes-page/details-formation/details-formation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    ThemesPageComponent,
+    DetailsFormationComponent
   ],
   imports: [
     BrowserModule,
---
+++ front/src/app/pages/themes-page/details-formation/details-formation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-formation',
+  templateUrl: './details-formation.component.html',
+  styleUrls: ['./details-formation.component.scss']
+})
+export class DetailsFormationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
