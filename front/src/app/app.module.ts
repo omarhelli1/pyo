40b080f6835d5e5c45fbf155fc221957259b7f24
Yas-Tech-Web-Fra <leroy.y1961@gmail.com>
2022-06-28 11:19:13 +0200

    feat: css avec cards et aside

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-// import  Formation from './models/formation.model';
-
+// design include
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { FooterComponentComponent } from './_commons/footer/footer-component/footer-component.component';
 import { FooterColComponentComponent } from './_commons/footer/footer-col-component/footer-col-component.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
-// FormationModel,
     FooterComponentComponent,
-    FooterColComponentComponent
+    FooterColComponentComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
