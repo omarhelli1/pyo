eef4d3975812a3e37d63db4e650375c9be7fce57
HELLI Omar <omarhelli@gmail.com>
2022-06-28 16:13:31 +0200

    modification de nav-bar et footer

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 import { ThemesPageComponent } from './pages/themes-page/themes-page.component';
+import { NotFoundComponent } from './_commons/not-found/not-found.component';
 
 const routes: Routes = [
-  {path: "liste-des-themes", component: ThemesPageComponent}
-
-];
+  {path: "liste-des-themes", component: ThemesPageComponent},
+  {path:'' , redirectTo: 'home', pathMatch: 'full'},
+  {  path:'home' , component: HomePageComponent},
+  {  path:'**' , component: NotFoundComponent}
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
