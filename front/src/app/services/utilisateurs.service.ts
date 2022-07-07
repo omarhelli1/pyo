4b59a036106d7de5fc77f9ba106b9392f523526c
Paul <decouxpaul1@gmail.com>
2022-07-07 15:22:32 +0200

    Feat: Création de la page /admin /admin/session/ admin/utilisateur

--- front/src/app/services/utilisateurs.service.ts
+++ front/src/app/services/utilisateurs.service.ts
@@ -1,8 +1,7 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
-
 // Interface
 import  UtilisateurModel from '../models/utilisateur.model'
 
@@ -11,23 +10,21 @@ import  UtilisateurModel from '../models/utilisateur.model'
 })
 export class UtilisateurService  {
 
+
+  private utilisateurSource = new BehaviorSubject(null);
+  currentUtilisateur = this.utilisateurSource.asObservable();
+
     constructor(private httpClient: HttpClient){ 
-        this.getAll()
+        this.httpClient.get('http://localhost:8080/utilisateurs').subscribe((utilisateurs: any) =>{
+            if(utilisateurs != null && utilisateurs.length > 0){
+                this.utilisateurSource.next(utilisateurs)
+            }
+        })    
     }
 
-    public getAll(){
-        this.httpClient.get('http://localhost:8080/utilisateurs').subscribe((utilisateurs: any) =>{
-            console.log(utilisateurs);
-            if(utilisateurs.length > 0){
-                
-                console.log(utilisateurs);
-                
-            }
-          })
-    }
+
 
     public inscriptionUtilisateur(user: UtilisateurModel){
-        console.log(user);
         user.entreprise = { 
             nom:'entreprise 1'
         }
